7682bf9c5c480114c57726550a044ac5b46d94e2
AbdoAitrais <abdo-pro@hotmail.com>
2023-03-26 14:06:03 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/data.json', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.sendFile(__dirname + '/city_graph.geojson');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
